de7874c29d09ab455d633def942bf4ae595767e1
Heinrich <heinrich@Heinrichs-MacBook-Pro.local>
2019-07-04 22:43:06 +0200

    sleep

--- get_next_line.c
+++ get_next_line.c
@@ -10,11 +10,46 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+int		ccpy(char **line, char *content, char c)
+{
+	int	i;
+
+	i = poschr(content, c);
+	if (!(*line = ft_strndup(content, i)))
+		return (0);
+	return (i);
+}
+
+t_list	*cur_lst(const int fd, t_list **readlist)
+{
+	t_list	*temp;
+
+	if (!readlist)
+		return	(NULL);
+	tmp = *readlist;
+	while (temp)
+	{
+		if (temp->content_size == fd)
+			return (temp);
+		temp = temp->next;
+	}
+	temp = ft_lstnew("", fd);
+	ft_lstadd(hlist, temp);
+	return (temp);
+}
+
 int	get_next_line(const int fd, char **line)
 {
-	char	buf[BUFF_SIZE + 1];
+	char		buf[BUFF_SIZE + 1];
+	static t_list	*readlist;
+	int		suc;
+	char		*temp;
+	t_list		*cur;
 
-	if (fd < 0 || !line)
+	if (fd < 0 || !line || read(fd,buf,50) < 0 || (!(cur = cur_lst(fd, &readlist))))
 		return (-1);
+	
 	return (1);
 }
